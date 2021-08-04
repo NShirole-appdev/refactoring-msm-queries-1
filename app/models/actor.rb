c931b6ac723b244ff851d39d359be9ab2860dcf3
Nina Shirole <ninashirole@uchicago.edu>
2021-08-04 03:18:48 +0000

    first points

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -11,4 +11,14 @@
 #  updated_at :datetime         not null
 #
 class Actor < ApplicationRecord
+  def characters
+    my_id = self.id
+
+    matching_movies = Movie.where({ :director_id => my_id })
+    matching_actors = Actor.where({ :id => the_id })
+    @actor_of_interest = Actor.where({ :id => @the_id }).at(0)
+    @filmography = Character.where({ :actor_id => @actor_of_interest.id })
+
+    return matching_movies
+  end
 end
